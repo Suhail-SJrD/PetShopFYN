9f238f919dc77cb638937e7669366be15bb0e019
Suhail S <155316593+Suhail-SJrD@users.noreply.github.com>
2026-02-14 11:47:42 +0530

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -4,11 +4,4 @@ module.exports = {
     android: {},
   },
   assets: ['./app/assets/fonts'],
-  dependencies: {
-    'react-native-vector-icons': {
-      platforms: {
-        ios: null,
-      },
-    },
-  },
 };
